7bbdcc3171b75ff814b7882cd56aa73b78dacf31
Shivani-Jadon <shivanijadon98@gmail.com>
2021-01-16 17:33:49 +0530

    store movies list in local storage

--- javascript/script.js
+++ javascript/script.js
@@ -4,20 +4,40 @@ let list1 = [];
 let list2 = [];
 let list3 = [];
 
+list1 = localStorage.getItem('movieListA');
+list1 = JSON.parse(list1);
+console.log("list 1 : ",list1);
+list2 = localStorage.getItem('movieListB');
+list2 = JSON.parse(list2);
+console.log("list 2 : ",list2);
+list3 = localStorage.getItem('movieListC');
+list3 = JSON.parse(list3);
+console.log("list 3 : ",list3);
+
 let movies_input = document.getElementsByClassName("textarea1");
-console.log(Array.isArray(movies_input));
+console.log(movies_input);
 
 for(let i = 0; i < movies_input.length; i++){
     
     console.log("value = ", movies_input[i].value);
     movies_input[i].onchange = function(event) {
         movies_input[i].value = event.target.value;
-        console.log(movies_input[i].value);
+        // console.log(movies_input[i].value);
 
         if(i == 0) {
             list1 = list1.concat(movies_input[0].value.split("\n"));
             list1 = [...new Set(list1)];
-            console.log(list1);
+            // console.log(list1);
+
+            list2.map((movie2, ind2) => {
+                list1.map((movie1, ind1) => {
+                    if(movie1 == movie2){
+                        list3.push(movie1);
+                        list1.splice(ind1,1);
+                        list2.splice(ind2,1);
+                    }
+                })
+            })
         }
 
         if(i == 1) {
@@ -36,51 +56,45 @@ for(let i = 0; i < movies_input.length; i++){
             })
         }
 
-            
-        console.log(list1);
-        console.log(list2);
-        console.log(list3);
     }
 
     movies_input[i].onfocus = function() {
+      
+        // clearing textbox on focus
         movies_input[i].value = "";
 
-        if(i == 1){
-            document.getElementById("list1").innerHTML = "";
 
-            for(let movie of list1){
-                let li = document.createElement("li");
-                li.innerText = movie;
-                document.getElementById("list1").appendChild(li);
-            }
+        // add movies to the list boxes
+        document.getElementById("list1").innerHTML = "";
+
+        for(let movie of list1){
+            let li = document.createElement("li");
+            li.innerText = movie;
+            document.getElementById("list1").appendChild(li);
+        }
+        
+        document.getElementById("list2").innerHTML = "";
+
+        for(let movie of list2){
+            let li = document.createElement("li");
+            li.innerText = movie;
+            document.getElementById("list2").appendChild(li);
         }
 
-        if(i == 0){
-
-            document.getElementById("list1").innerHTML = "";
-
-            for(let movie of list1){
-                let li = document.createElement("li");
-                li.innerText = movie;
-                document.getElementById("list1").appendChild(li);
-            }
-            
-            document.getElementById("list2").innerHTML = "";
-
-            for(let movie of list2){
-                let li = document.createElement("li");
-                li.innerText = movie;
-                document.getElementById("list2").appendChild(li);
-            }
-
-            document.getElementById("list3").innerHTML = "";
-            
-            for(let movie of list3){
-                let li = document.createElement("li");
-                li.innerText = movie;
-                document.getElementById("list3").appendChild(li);
-            }
+        document.getElementById("list3").innerHTML = "";
+        
+        for(let movie of list3){
+            let li = document.createElement("li");
+            li.innerText = movie;
+            document.getElementById("list3").appendChild(li);
         }
+
+        
+        localStorage.setItem('movieListA', JSON.stringify(list1));
+        localStorage.setItem('movieListB', JSON.stringify(list2));
+        localStorage.setItem('movieListC', JSON.stringify(list3));
+
         
     }    
 }
+
